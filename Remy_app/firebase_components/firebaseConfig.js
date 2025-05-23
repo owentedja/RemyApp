ab3e6b212281afe08580b2b1398830f1d6e6968b
owentedja <139730187+owentedja@users.noreply.github.com>
2025-05-23 01:09:30 -0700

    Update firebaseConfig.js
    protecting backup file

--- Remy_app/firebase_components/firebaseConfig.js
+++ Remy_app/firebase_components/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your Firebase web app's configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6N9jr9ESmi__8rhom1Pgm6lj7wmkjL3A",
-  authDomain: "remy-login-data.firebaseapp.com",
-  projectId: "remy-login-data",
-  storageBucket: "remy-login-data.firebasestorage.app",
-  messagingSenderId: "563905398924",
-  appId: "1:563905398924:web:5a445c5ae648bcb25013a9",
-  measurementId: "G-JRDY5C0F11"
+  apiKey: ""###################################",",
+  authDomain: ""###################################",",
+  projectId: ""###################################",",
+  storageBucket: ""###################################",",
+  messagingSenderId: ""###################################",",
+  appId: ""###################################",",
+  measurementId: ""###################################","
 };
 
 // Initialize Firebase
